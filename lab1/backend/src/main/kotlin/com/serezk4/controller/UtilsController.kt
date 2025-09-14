13acfb71e3ae7d6378d9be1f16956ef563308372
serezk4 <serezadorohin@gmail.com>
2025-09-14 03:37:56 +0300

    feat(spec): updated spec

--- lab1/backend/src/main/kotlin/com/serezk4/controller/UtilsController.kt
+++ lab1/backend/src/main/kotlin/com/serezk4/controller/UtilsController.kt
@@ -2,8 +2,9 @@ package com.serezk4.controller
 
 import com.serezk4.api.api.UtilsApi
 import com.serezk4.api.model.CreateTestObjects200Response
+import com.serezk4.api.model.GroupedByCreatureTypeDto
+import com.serezk4.api.model.ObjectsPerUserStatsDto
 import com.serezk4.service.ObjectsService
-import io.github.resilience4j.ratelimiter.annotation.RateLimiter
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RestController
 
@@ -12,21 +13,27 @@ class UtilsController(
     private val objectsService: ObjectsService
 ) : UtilsApi {
 
-    @RateLimiter(name = "default")
-    override fun deleteObjectsByTimezone(timezone: Int): ResponseEntity<Unit> {
-        objectsService.deleteObjectsByTimezone(timezone)
-        return ResponseEntity.noContent().build()
-    }
-
-    @RateLimiter(name = "default")
-    override fun deleteOneByGovernment(government: String): ResponseEntity<Unit> {
-        objectsService.deleteOneByGovernment(government)
-        return ResponseEntity.noContent().build()
-    }
-
-    @RateLimiter(name = "test")
     override fun createTestObjects(): ResponseEntity<CreateTestObjects200Response> {
-        objectsService.createTestObject()
-        return ResponseEntity.ok().body(CreateTestObjects200Response(true))
+        return super.createTestObjects()
+    }
+
+    override fun deleteByAttackLevel(attackLevel: Long): ResponseEntity<Unit> {
+        return super.deleteByAttackLevel(attackLevel)
+    }
+
+    override fun distributeRings(): ResponseEntity<Unit> {
+        return super.distributeRings()
+    }
+
+    override fun getObjectsPerUserStats(): ResponseEntity<List<ObjectsPerUserStatsDto>> {
+        return super.getObjectsPerUserStats()
+    }
+
+    override fun getUniqueDefenseLevels(): ResponseEntity<List<Float>> {
+        return super.getUniqueDefenseLevels()
+    }
+
+    override fun groupByCreatureType(): ResponseEntity<List<GroupedByCreatureTypeDto>> {
+        return super.groupByCreatureType()
     }
 }
